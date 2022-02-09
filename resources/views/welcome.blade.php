d76b7c9b466a74a595992e315ffcf93cac157593
eagle <osu.eagle@gmail.com>
2022-02-09 17:48:20 +0200

    migration, model, routes, home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,9 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">My first Lara site
+                <div><a href="/tasks">All tasks</a></div>
+                </div>
             </div>
         </div>
     </body>
